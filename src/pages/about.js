25e8f6cb8b3539b30f6117e4954dcf4fc596e1b0
Vasyl Dizhak <vasyl.dizhak@moneypark.com>
2021-04-26 16:03:14 +0200

    Check vercel build

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const AboutPage = () => {
       <p>I'm a software engineer working as a Project Lead at <a target="_blank" rel="noreferrer" href="https://moneypark.ch">MoneyPark</a> focusing on Python/Django stack.</p>
       <p>For the last 14 years, I've been managing, developing, testing, and maintaining software applications and projects in areas of
         Fintech, GeoSpatial, Healthcare, and Social Networking.</p>
-      <p>I love GIS and occasionally contribute to open-source projects.</p>
+      <p>I love GIS and occasionally contribute to open-source projects. </p>
 
       <p>You can reach me by e-mail: <a href="mailto:hi@dizhak.com">hi@vasyl.dizhak</a> or on twitter <a target="_blank" rel="noreferrer" href="https://twitter.com/rootart">@rootart</a></p>
     </Layout>
